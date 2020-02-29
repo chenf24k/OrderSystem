986749afa9f62590252bda7a2d3f80b5ea6b1716
陈枫 <chenfeng@xiaoma.cn>
2020-03-01 04:17:24 +0800

    1.日志文件配置 2.yml数据库配置 3.买家类目dao实现

---
+++ src/main/java/com/imooc/sell/dataobject/ProductCategory.java
@@ -0,0 +1,44 @@
+package com.imooc.sell.dataobject;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 表：
+ * product_category
+ */
+// 如果表为s_product_category，可以使用这个形式匹配
+//@Table(name="s_product_category")
+
+@Entity
+@DynamicUpdate
+@Data
+@NoArgsConstructor
+public class ProductCategory implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer categoryId;
+    private String categoryName;
+    private Integer categoryType;
+    private Date createTime;
+    private Date updateTime;
+
+    public ProductCategory(String categoryName, Integer categoryType) {
+        this.categoryName = categoryName;
+        this.categoryType = categoryType;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                ", categoryType=" + categoryType +
+                '}';
+    }
+}
---
+++ src/test/java/com/imooc/sell/repository/ProductCategoryRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.imooc.sell.repository;
+
+import com.imooc.sell.dataobject.ProductCategory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductCategoryRepositoryTest {
+    @Autowired
+    private ProductCategoryRepository repository;
+
+    @Test
+    public void findOneTest() {
+        ProductCategory productCategory = null;
+        Optional<ProductCategory> op = repository.findById(1);
+        if (op.isPresent()) {
+            productCategory = op.get();
+            System.out.println(productCategory.toString());
+        }
+    }
+
+    @Test
+    @Transactional
+    public void saveTest() {
+        ProductCategory productCategory = new ProductCategory("特色", 7);
+        ProductCategory result = repository.save(productCategory);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    @Transactional
+    public void updateTest() {
+        ProductCategory productCategory = null;
+        Optional<ProductCategory> op = repository.findById(2);
+        if (op.isPresent()) {
+            productCategory = op.get();
+            productCategory.setCategoryType(4);
+        }
+        repository.save(productCategory);
+    }
+
+    @Test
+    public void findByCategoryTypeInTest() {
+        List<Integer> list = Arrays.asList(4, 6, 10);
+        List<ProductCategory> result = repository.findByCategoryTypeIn(list);
+        System.out.println(result.toString());
+        Assertions.assertNotEquals(0, result.size());
+    }
+
+}
